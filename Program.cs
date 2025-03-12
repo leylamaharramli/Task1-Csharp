58c742b25782b11fddd7e89c62f5fdc95aab5524
leylamaharramli <leyla@example.com>
2025-03-12 05:27:01 +0400

    changed1 Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿#region
+string username = Console.Readline();
+string password = Console.Readline();
+
+if (username == "Admin" && password == "Admin123")
+{
+    Console.Writeline("xosh gelmisiniz");
+}
+else
+{
+    Console.Writeline("username ve ya password yanlisdir");
+}
+#endregion
