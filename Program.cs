7994849fef9d52489f96a7e76d989c50101adda0
leylamaharramli <leyla@example.com>
2025-03-12 05:31:51 +0400

    changed2 Program.cs

--- Program.cs
+++ Program.cs
@@ -11,3 +11,17 @@ else
     Console.Writeline("username ve ya password yanlisdir");
 }
 #endregion
+
+#region
+int num = Convert.ToInt32(Console.Readline());
+
+if ( num % 2 == 0)
+{
+    Console.Writeline("eded cutdur");
+}
+else
+{
+    Console.Writeline("eded tekdir");
+}
+#endregion
+
